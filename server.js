11352117b49dd688b8d46a79e192fb7c876c1bdb
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2020-09-25 15:56:59 +0530

    Remove all views

--- server.js
+++ server.js
@@ -38,49 +38,12 @@ app.post("/createShortUrl", async (req, res) => {
   firestoreOperations.saveRecord(newRecord).then(() => res.send(newRecord));
 });
 
-app.post("/filter/", async (req, res) => {
-  const { filter } = req.body;
-  firestoreOperations.fetchAll().then((snapshot) => {
-    const filteredRecord = snapshot.docs.filter((rec) =>
-      rec.data().fullUrl.includes(filter)
-    );
-    // res.render("index", {
-    //   records: filteredRecord,
-    //   sort: 1,
-    //   filterValue: filter,
-    // });
-  });
-});
-
-app.post("/clearFilter/", async (req, res) => {
-  // res.redirect("/");
-});
-
-app.get("/sort/:sortField/:order", async (req, res) => {
-  console.log(req.params);
-  const { sortField, order } = req.params;
-  const fetchOrder = order == 1 ? "asc" : "desc";
-
-  firestoreOperations
-    .fetchOneByOrder(sortField, fetchOrder)
-    .then((snapshot) => {
-      // res.render("index", {
-      //   records: snapshot.docs,
-      //   sort: order,
-      //   filterValue: null,
-      // });
-    });
-});
-
 app.get("/:shortUrl", async (req, res) => {
   const { shortUrl } = req.params;
-  console.log("shortUrl", shortUrl);
   if (shortUrl === "favicon.ico") return res.send("");
   firestoreOperations.fetchOne(shortUrl).then((doc) => {
     const { fullUrl } = doc.data();
-    console.log("doc.data()", fullUrl);
     res.redirect(fullUrl);
-    res.send("Hi");
   });
 });
 
